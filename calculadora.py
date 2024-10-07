3ab5c1e073295e603a6ab800e84ef45859411b91
Joao <producoespjomal@gmail.com>
2024-10-07 11:47:26 -0300

    feat: implementa a subtracao

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,7 @@
 def somar(operando1, operando2):
     print(f"Somando {operando1} + {operando2}")
     return operando1 + operando2
+
+def subtrair(operando1, operando2):
+    print(f"Subtraindo {operando1} + {operando2}")
+    return operando1 - operando2

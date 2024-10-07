2ef19cc1e9d1dcc5989ddfac586e63b2271f3f4e
Joao <producoespjomal@gmail.com>
2024-10-07 15:08:02 -0300

    feat: adiciona um operando à soma

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-def somar(operando1, operando2):
-    print(f"Somando {operando1} + {operando2}")
-    return operando1 + operando2
+def somar(operando1, operando2, operando3):
+    print(f"Somando {operando1} + {operando2} + {operando3}")
+    return operando1 + operando2 + operando3
 
 def subtrair(operando1, operando2):
     print(f"Subtraindo {operando1} + {operando2}")
